3675920c7ff91f2c3492259721a0e0d512801d6f
fdemir <frndemir@gmail.com>
2022-10-27 21:09:20 +0300

    add welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+
 const Home = () => {
-  return <div className="bg-red-500">testing some stuff</div>;
+  return (
+    <div className="h-screen grid place-items-center bg-slate-100">
+      <Image
+        className="z-10 "
+        src="/planet.png"
+        width={682}
+        height={723}
+        alt="Planet"
+      />
+      <h1 className="text-[18rem] absolute z-0 ">Varuna</h1>
+    </div>
+  );
 };
 
 export default Home;
